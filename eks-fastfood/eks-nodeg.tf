00f92b802ec92e0f5765f3d51a177a5190368718
Marcelo Ebert <ebert.marcelo@gmail.com>
2024-12-05 00:36:20 -0300

    ajustes

--- eks-fastfood/eks-nodeg.tf
+++ eks-fastfood/eks-nodeg.tf
@@ -7,7 +7,7 @@ resource "aws_eks_node_group" "node-group" {
   instance_types   = [var.instanceType]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     max_size     = 5
     min_size     = 1
   }
