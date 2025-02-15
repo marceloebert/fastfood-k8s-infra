dc2d133c27614f8d4333751aac47dd0bcd5dd7b9
Marcelo Ebert <50593482+marceloebert@users.noreply.github.com>
2025-02-15 16:33:47 -0300

    Update s3.tf

--- eks-fastfood/s3.tf
+++ eks-fastfood/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "eks_state" {
-  bucket = "eks-fastfood-terraform-state"
+  bucket = "eks-fastfood-order-terraform-state"
 
   tags = {
     Name        = "EKS Fastfood Terraform State"
